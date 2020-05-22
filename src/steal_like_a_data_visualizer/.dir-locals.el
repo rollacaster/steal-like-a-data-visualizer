b2e246a5fc3649e27aa572825b155f3291b1364c
Thomas Sojka <thsojka@web.de>
2020-05-22 08:14:15 +0200

    Update Emacs settings

--- src/steal_like_a_data_visualizer/.dir-locals.el
+++ src/steal_like_a_data_visualizer/.dir-locals.el
@@ -1,5 +1,8 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((clojurescript-mode
+((clojurec-mode
+  (cider-default-cljs-repl . figwheel-main))
+ (clojurescript-mode
   (cider-default-cljs-repl . figwheel-main)))
+
